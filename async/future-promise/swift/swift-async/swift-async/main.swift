0986d88fe49987a2ffcdc6e142933a25188773bb
Alan Uthoff <blackcloak@gmail.com>
2021-01-25 13:48:30 -0600

    Initial future-promise Swift 5 Example

---
+++ async/future-promise/swift/swift-async/swift-async/main.swift
@@ -0,0 +1,44 @@
+/*
+Copyright 2021 Alan Uthoff
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import Foundation
+
+class AsyncClass : NSObject
+{
+    func simulatedAsyncWork( workTimeInMsec:  UInt, withDispatchSemaphore sNotify: DispatchSemaphore) {
+        let que = DispatchQueue.global(qos: .userInitiated)
+        que.async {
+            print("Doing Async Work For %lu ms",workTimeInMsec)
+            Thread.sleep(forTimeInterval:TimeInterval(workTimeInMsec/1000))
+            print("Async Work Done")
+            sNotify.signal()
+        }
+    }
+        
+    func  asyncFunction( message: String) {
+        let sema  = DispatchSemaphore(value: 0)
+        simulatedAsyncWork(workTimeInMsec: 1000, withDispatchSemaphore: sema)
+        sema.wait()
+        print("Message From asyncFunction: %@ ",message)
+           
+    }
+    
+}
+
+let obj =  AsyncClass.init()
+obj.asyncFunction(message: "Hello World")
+
